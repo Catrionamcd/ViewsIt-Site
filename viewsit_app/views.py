f9ebb91238eefda823abc080c1d5d887908ffb5b
Catrionamcd <catrionamcd@gmail.com>
2021-10-25 17:44:21 +0000

    Create a class view for list of channels

--- viewsit_app/views.py
+++ viewsit_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.utils.text import slugify
+from django.views import generic, View
+from .models import Channel
+from cloudinary.forms import cl_init_js_callbacks
 
-# Create your views here.
+class ChannelList(generic.ListView):
+    model = Channel
+    queryset = Channel.objects.filter(status=1).order_by("-created_on")
+    
+    template_name = "channel_list.html"
--- viewsit_proj/settings.py
+++ viewsit_proj/settings.py
@@ -88,17 +88,16 @@ WSGI_APPLICATION = 'viewsit_proj.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # if development == 'True':
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-# # else:
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
+# # else:
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
